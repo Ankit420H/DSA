3b012a02e969a3d05a806c828777493fad32128a
ANKIT KUMAR YADAV (Junior) <112342950+Ankit420H@users.noreply.github.com>
2025-12-20 16:43:19 +0530

    Time: 19 ms (85.28%), Space: 17.6 MB (98.05%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.py
+++ 1399-count-largest-group/1399-count-largest-group.py
@@ -1,20 +1,19 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        d = {}
-        for i in range(1, n + 1):
-            x = i
-            s = 0
+        d={}
+        for i in range(1,n+1):
+            x=i
+            s=0
             while x:
-                s += x % 10
-                x //= 10
-            d[s] = d.get(s, 0) + 1
-
-        m = 0
-        r = 0
+                s+=x%10
+                x//=10
+            d[s]=d.get(s,0)+1
+        m=0
+        r=0
         for v in d.values():
-            if v > m:
-                m = v
-                r = 1
-            elif v == m:
-                r += 1
+            if m<v:
+                m=v
+                r=1
+            elif m==v:
+                r+=1
         return r
